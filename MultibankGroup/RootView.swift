7ab78120fdf861dc42df20dd0837828074c540cc
Djuro Alfirevic <djalfirevic@gmail.com>
2026-03-04 18:30:10 +0200

    Update RootView

--- MultibankGroup/RootView.swift
+++ MultibankGroup/RootView.swift
@@ -9,10 +9,26 @@ import SwiftUI
 
 struct RootView: View {
 
+    // MARK: - Properties
+
+    @StateObject private var service = WebSocketService()
+    @StateObject private var coordinator: RootCoordinator
+
+    // MARK: - Initialization
+
+    init() {
+        let service = WebSocketService()
+        _service = StateObject(wrappedValue: service)
+        _coordinator = StateObject(wrappedValue: RootCoordinator(service: service))
+    }
+
     // MARK: - View
 
     var body: some View {
-        Color.white
+        coordinator.start()
+            .onOpenURL { url in
+                coordinator.handleDeepLink(url)
+            }
     }
 }
 
